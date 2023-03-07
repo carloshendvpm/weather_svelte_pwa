4aa36e7ec577ec77659f0ce749e544a1203c944e
Carlos Henrique <hencarlosdv@gmail.com>
2023-03-07 12:13:17 -0300

    create card for weather info

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,25 @@
+<script>
+  import sun from '../images/sun.svg';
+</script>
+<div class="bg-zinc-800 w-1/4 flex flex-col p-5 rounded pb-10">
+  <div class="header w-full flex justify-between items-center mb-10">
+    <div class="text-white text-xl">
+      <p class="font-bold text-2xl">Recife, PE</p>
+      <p class="font-normal">broken clouds</p>
+    </div>
+    <img src={sun} class="w-1/5" alt="sun img">
+  </div>
+  <div class="flex w-full justify-between items-center">
+    <p class="text-8xl font-semibold text-white">26ºC</p>
+    <div class="details text-white font-medium">
+      <h3>Details</h3>
+      <p>Feels Like: 26.02ºC</p>
+      <p>Humidity: 11%</p>
+      <p>Pressure: 1013hPs</p>
+    </div>
+  </div>
+</div>
+
+<style>
+
+</style>
