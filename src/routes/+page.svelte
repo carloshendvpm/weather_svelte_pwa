a82b46cae7263dcc77e94fea98715940967594a8
Carlos Henrique <hencarlosdv@gmail.com>
2023-03-07 13:37:11 -0300

    add prop icon to card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script>
 	import Card from "$lib/components/Card.svelte";
+	import sun from "$lib/images/sun.png"
 </script>
 
-<div class="w-full h-screen flex items-center justify-center">
-	<Card/>
+<div class="w-screen h-screen flex items-center justify-center">
+	<Card icon={sun} />
 </div>
