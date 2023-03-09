4848b9567ad644463139b54277b441fb00d59ddd
Carlos Henrique <hencarlosdv@gmail.com>
2023-03-09 15:55:57 -0300

    removed icon for spacing

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,5 +1,6 @@
 <script>
   import humidty_icon from '$lib/images/humidity.svg';
+  import rain_icon from '$lib/images/rain.svg';
 </script>
 
 <div class="bg-zinc-800 flex flex-col text-white w-full max-w-xl p-8 border-4 rounded-lg">
@@ -10,14 +11,19 @@
       <p class="font-medium text-xl">Recife, PE</p>
     </div>
   </div>
-  <div>
-    <div class="humidity flex items-center w-2/5 bg-zinc-600 bg-opacity-40 justify-center pr-2 md:pr-0 max-w-xs rounded-sm gap-2 md:gap-5">
-      <div>
-        <img src={humidty_icon} alt="humidity" class="w-10 h-1/3">
-      </div>
+  <div class="flex flex-row justify-between">
+    <div class="humidity flex items-center w-1/3 bg-zinc-600 bg-opacity-40 justify-center p-3 md:pr-0 rounded-sm gap-2 md:gap-5">
       <div class="font-medium">
         <p>Humidity</p>
         <p>17%</p>
+      </div>
+    </div>
+    <div>
+      <div class="rain ">
+        <div class="font-medium">
+          <p>Rain</p>
+          <p>17%</p>
+        </div>
       </div>
     </div>
   </div>
