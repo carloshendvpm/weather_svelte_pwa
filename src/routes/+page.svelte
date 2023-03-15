41f88a1ea44528a2f2fac9b726609fcd6375bd24
Carlos Henrique <hencarlosdv@gmail.com>
2023-03-15 10:10:23 -0300

    refactoring import to use $lib instead normal import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Card from "$lib/components/Card.svelte";
-	import DailyForecast from "../lib/components/DailyForecast.svelte";
+	import DailyForecast from "$lib/components/DailyForecast.svelte";
 </script>
 
 <div class="min-h-screen w-screen flex flex-col items-center justify-center px-5">
