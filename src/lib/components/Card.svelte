a5ee049d00532b12fa99d4728f419b3ace91a2b8
Carlos Henrique <hencarlosdv@gmail.com>
2023-03-15 11:01:32 -0300

    add variables to main weather card data

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -3,14 +3,17 @@
   import rain from '$lib/images/rain.svg';
   import sun from '$lib/images/sunny.svg'
   import InfoWeather from './InfoWeather.svelte';
+  let location = "Recife, PE"
+  let temperature = "29ºC"
+  let date = "15, mar 2023."
 </script>
 
 <div class="bg-zinc-800 flex flex-col text-white w-full max-w-xl p-8 drop-shadow-lg rounded-lg">
   <div class="header flex justify-between mb-8">
     <div>
-      <p class="font-medium text-xl"> Recife, PE</p>
-      <p class="text-6xl font-semibold mb-1">29ºC</p>
-      <p class="mb-3">14 mar, 2023.</p>
+      <p class="font-medium text-xl">{location}</p>
+      <p class="text-6xl font-semibold mb-1">{temperature}</p>
+      <p class="mb-3">{date}</p>
     </div>
     <div class="flex justify-end m-2 items-start w-1/5">
       <img src={rain} alt="rain" class="text-center">
