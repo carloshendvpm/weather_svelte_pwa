621400ec2a6cfb2e3e2d185ff3fc7f2a746a5933
Carlos Henrique <hencarlosdv@gmail.com>
2023-03-15 12:09:08 -0300

    add tests to the components entrance

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,19 @@
 <script>
 	import Card from "$lib/components/Card.svelte";
 	import DailyForecast from "$lib/components/DailyForecast.svelte";
-	import Search from "../lib/components/Search.svelte";
+	import Search from "$lib/components/Search.svelte";
+	import { fly } from 'svelte/transition'
+
+	let searched = false
+	setTimeout(() => {
+		searched = true
+	}, 2000);
 </script>
 
-<div class="min-h-screen w-screen flex flex-col items-center justify-center px-5 bg-zinc-700">
+<div class="transition-opacity duration-500 ease-in-out min-h-screen w-screen flex flex-col items-center justify-center px-5 bg-zinc-700 overflow-hidden">
 	<Search/>
-	<Card/>
-	<DailyForecast/>
+	{#if searched}
+		<Card/>
+		<DailyForecast/>
+	{/if}
 </div>
