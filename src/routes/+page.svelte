75776e481712e6374cb6fd13e58a123620d9cc58
Carlos Henrique <hencarlosdv@gmail.com>
2023-03-15 10:05:41 -0300

    add daily forecast to the main weather page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Card from "$lib/components/Card.svelte";
+	import DailyForecast from "../lib/components/DailyForecast.svelte";
 </script>
 
 <div class="min-h-screen w-screen flex flex-col items-center justify-center px-5">
 	<Card/>
+	<DailyForecast/>
 </div>
