2c5432cbb0f48998e8094b034b38ada0ff8f09de
Carlos Henrique <hencarlosdv@gmail.com>
2023-03-15 10:09:35 -0300

    create InfoWeather component to display info data on the card component

---
+++ src/lib/components/InfoWeather.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let percentage;
+  export let icon;
+  export let type;
+</script>
+<div class="bg-zinc-800 drop-shadow-2xl shadow-[0px_22px_20px_4px_rgba(0,0,0,0.50)] transition duration-300 ease-in-out hover:scale-110 w-40 p-2.5 flex flex-col items-center gap-1 rounded border-zinc-400/50 text-center">
+  <img src={icon} alt="" class="w-5">
+  <p class="font-semibold text-sm">{percentage}</p>
+  <p class="font-normal text-xs">{type}</p>
+</div>
